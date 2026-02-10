86cf69ed069f9d2a56ddbde553875752c03bcaa4
NDAYISABA KAMARIZA Belie <104085086+NKBelie@users.noreply.github.com>
2026-02-10 18:22:04 +0200

    User Profile

---
+++ QuestionBonus-UserProfile-Api/UserProfile/src/main/java/userprofile/rw/UserProfile/UserProfile.java
@@ -0,0 +1,13 @@
+package userprofile.rw.UserProfile;
+
+public class UserProfile {
+    Long userId;
+    String username;
+    String email;
+    String fullName;
+    int age;
+    String country;
+    String bio;
+    boolean active;
+
+}
---
+++ QuestionBonus-UserProfile-Api/UserProfile/src/test/java/userprofile/rw/UserProfile/UserProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package userprofile.rw.UserProfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
